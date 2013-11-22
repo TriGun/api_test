0ec26dbe8b12836f278159aa6d66a2bfeed7ab16
alex <assasinbox@gmail.com>
2013-11-22 17:12:06 +0200

    encrypt post data

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 require_once('api.php');
 require_once('db.php');
+require_once('aes.php');
+
+$EN_KEY = '4375NDYHBHBK34534BL534B534';
 
 if(isset($_POST['q'])){
 
-    parse_str(base64_decode($_POST['q']), $data);
+    parse_str(AesCtr::decrypt(base64_decode($_POST['q']), $EN_KEY, 128), $data);
 
     if(isset($data['action'])){
 
@@ -15,13 +18,8 @@ if(isset($_POST['q'])){
         $api = new Api();
         $api->do_action($data);
 
-        echo base64_encode(json_encode( $api->toArray() ));
+        echo base64_encode(AesCtr::encrypt( json_encode( $api->toArray() ), $EN_KEY, 128));
 
     }
 
 }
-
-
-
-
-
